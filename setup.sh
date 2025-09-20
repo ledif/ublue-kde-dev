13699840c665efae30ecb180882eb19c7782d295
Adam Fidel <adam@fidel.id>
2025-09-19 22:52:52 -0500

    fix: use /var/local directly

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 #
 # Should be run on host
 
-set -euo pipefail
+set -exuo pipefail
 
 sudo mkdir -p /var/local/kde-dev/{home,kde}
 sudo chown $(id -u):$(id -u) /var/local/kde-dev/{home,kde}
@@ -10,5 +10,12 @@ sudo chown $(id -u):$(id -u) /var/local/kde-dev/{home,kde}
 mkdir -p /var/local/kde-dev/home/.config
 cp ./kde-builder.yaml /var/local/kde-dev/home/.config
 
-distrobox create --name kde-dev --home /var/local/kde-dev/home --init --additional-packages "systemd" --pull --image ghcr.io/ublue-os/fedora-toolbox:latest
+distrobox create \
+	--name kde-dev \
+	--home /var/local/kde-dev/home \
+	--volume /var/local/kde-dev/kde:/var/local/kde-dev/kde:Z \
+	--init \
+	--additional-packages "systemd" \
+	--pull \
+	--image ghcr.io/ledif/ublue-kde-dev:latest
 
